5aba84e7b0a07a35b3efcfe88bd6ebea8910e207
Chen zhulin <2655310177@qq.com>
2019-03-12 15:04:35 +0800

    页面样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> @yield('title','Weibo App') -- Laravel 新手入门教程，正式开启的一段咸鱼之路</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+
+
+<div class="container">
+    @yield('content')
+    @include('layouts._footer')
+</div>
 </body>
 </html>
