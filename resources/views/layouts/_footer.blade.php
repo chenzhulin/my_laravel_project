c21c479a7d62d03addbcbbca206ba7771b59949f
Chen zhulin <2655310177@qq.com>
2019-03-12 14:49:09 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+<img src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" class="brand-icon">
+    <a href="https://learnku.com/laravel/courses">刻意练习每日精进</a>
+
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
