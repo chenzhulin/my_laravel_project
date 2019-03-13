49144476c1503e3c6492f3924d19d160ae764f41
Chen zhulin <2655310177@qq.com>
2019-03-13 11:31:18 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Auth;
 class UsersController extends Controller
 {
     public function create()
@@ -29,9 +29,8 @@ class UsersController extends Controller
              'email'=>$request->email,
             'password'=>bcrypt($request->password),
          ]);
-
+         Auth::login($user);
          session()->flash('success','欢迎，您将在这里开启一段新的旅程！');
-
          return redirect()->route('users.show',[$user]);
 
 
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,5 +13,6 @@
     @yield('content')
     @include('layouts._footer')
 </div>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
