8f4ae720644dac885d45c24278851a797f8e2776
Chen zhulin <2655310177@qq.com>
2019-03-13 11:41:42 +0800

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -22,6 +22,13 @@
                             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
                         </div>
 
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+                                <label for="exampleCheck1" for="form-check-input">记住我</label>
+                            </div>
+                        </div>
+
                         <button type="submit" class="btn btn-primary">登录</button>
                     </form>
 
