3619c3108e29d953726c342bd3836235b728e711
Chen zhulin <2655310177@qq.com>
2019-03-13 15:21:48 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 use Auth;
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' =>['show','create','store'],
+        ]);
+        $this->middleware('guest',[
+            'only'  =>['create'],
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -31,16 +42,19 @@ class UsersController extends Controller
          ]);
          Auth::login($user);
          session()->flash('success','欢迎，您将在这里开启一段新的旅程！');
+
          return redirect()->route('users.show',[$user]);
     }
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update( User $user,Request $request)
     {
+        $this->authorize('update',$user);
         $this->validate($request,[
             'name'  =>'required|max:50',
             'password'=>'nullable|confirmed|min:6',
@@ -55,6 +69,6 @@ class UsersController extends Controller
 
        $user->update($data);
         session()->flash('success','个人资料更新成功！');
-        return redirect()->route('users.show',$user);
+        return redirect()->route('users.show',$user->id);
     }
 }
