957aa3583da23983dc735165ea075f6bceb4c037
Chen zhulin <2655310177@qq.com>
2019-03-14 09:21:14 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,9 +27,14 @@ class SessionsController extends Controller
         ]);
 
         if (Auth::attempt($credentials,$request->has('remember'))){
-            session()->flash('success','欢迎回来');
-            $fallback = route('users.show',[Auth::user()]);
-            return redirect()->intended($fallback);
+            if (Auth::user()->activated){
+                session()->flash('success','欢迎回来');
+                $fallback = route('users.show',[Auth::user()]);
+                return redirect()->intended($fallback);
+            }else{
+                Auth::logout();
+                session()->flash('success','你的账号未激活，请检查邮箱中的邮件进行注册。');
+            }
         }else{
             session()->flash('danger','您的邮箱和密码有误！');
             return redirect()->back()->withInput();
