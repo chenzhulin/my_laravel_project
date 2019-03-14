8f84893f25b7184b837f21809a6289acf24df4d1
Chen zhulin <2655310177@qq.com>
2019-03-14 16:57:20 +0800

    发布微博

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -33,7 +33,7 @@ class UsersController extends Controller
     public function show(User $user)
     {
         $statuses = $user->statuses()
-                            ->orderBy('created_at')
+                            ->orderBy('created_at','desc')
                             ->paginate(30);
         return view('users.show',compact('user','statuses'));
     }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 class Status extends Model
 {
-
+    protected $fillable = ['content'];
     public function user()
     {
         return $this->belongsTo(User::class);
