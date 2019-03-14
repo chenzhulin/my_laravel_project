abaa89d24e6c658cfb9fb48407de541177f3680b
Chen zhulin <2655310177@qq.com>
2019-03-14 16:05:01 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
             $user->activation_token = str_random(30);
         });
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
