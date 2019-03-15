77f4f51a6f618f4a4713ef026dbf55c0632dae06
Chen zhulin <2655310177@qq.com>
2019-03-15 09:57:51 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,39 @@ class User extends Authenticatable
         return $this->statuses()
                 ->orderBy('created_at','desc');
     }
+
+    //粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    //关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if (! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followers()->sync('user_ids',false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if (! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach();
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
+    }
 }
